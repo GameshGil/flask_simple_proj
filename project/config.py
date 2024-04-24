6178845e8181358f1cecf15d2aee582871b362ba
GameshGil <s.amirov90@mail.ru>
2024-04-24 21:47:00 +0300

    Update: Develop config added

---
+++ project/config.py
@@ -0,0 +1,15 @@
+"""Flask config"""
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    FLASK_APP = 'application.py'
+    SECRET_KEY = getenv('SECRET_KEY')
+
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///project_db.db'
