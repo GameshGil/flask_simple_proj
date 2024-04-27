bd628263f1416fe0d40b6996b6448315fdaf32eb
GameshGil <s.amirov90@mail.ru>
2024-04-27 09:29:43 +0300

    Update: forms for registration and login created

---
+++ project/my_blog/forms.py
@@ -0,0 +1,31 @@
+"""Forms for main Flask app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField, BooleanField
+from wtforms import SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class RegistrationForm(FlaskForm):
+    """User registration form"""
+    username = StringField('ФИО пользователя', validators=[DataRequired()])
+    email = EmailField('Email пользователя', validators=[DataRequired()])
+    role = SelectField(
+        'Роль',
+        choices=[
+            ('user', 'Пользователь'),
+            ('manager', 'Менеджер'),
+            ('admin', 'Администратор')
+        ],
+        validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_repeat = PasswordField(
+        'Введите пароль повторно', validators=[DataRequired()])
+    submit = SubmitField('Подтвердить')
+
+
+class LoginForm(FlaskForm):
+    """User login form"""
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
