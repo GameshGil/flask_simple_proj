b754ce648f9999a1e9ef3be93f04bb924b0cedd0
GameshGil <s.amirov90@mail.ru>
2024-04-27 09:41:24 +0300

    Update: app routes with index page added

---
+++ project/my_blog/routes.py
@@ -0,0 +1,22 @@
+"""Routes for Flask app"""
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import current_app as app
+from flask import render_template, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+from sqlalchemy import select
+
+from my_blog import login_manager
+from my_blog.models import db, User, Post
+from my_blog.forms import RegistrationForm, LoginForm
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = db.session.get(User, user_id)
+    return user
+
+
+@app.route('/')
+def index():
+    """Index page for Flask app."""
+    return render_template('index.html')
